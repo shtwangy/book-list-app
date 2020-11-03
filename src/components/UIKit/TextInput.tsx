dc2cad7158444bbfe88943b7b53d1cdbf770c825
shtwangy <shtwangy@gmail.com>
2020-11-03 20:39:12 +0900

    feat(src/components/UIKit/TextInput.tsx): create text input component.
    Issues #1

---
+++ src/components/UIKit/TextInput.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+
+type TextInputProps = {
+    fullWidth: boolean;
+    label: string;
+    multiline: boolean;
+    required: boolean;
+    rows: number;
+    value: string;
+    type: React.InputHTMLAttributes<unknown>['type'];
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const TextInput = (props: TextInputProps) => {
+    return (
+        <TextField
+            fullWidth={props.fullWidth}
+            label={props.label}
+            margin="dense"
+            multiline={props.multiline}
+            required={props.required}
+            rows={props.rows}
+            value={props.value}
+            type={props.type}
+            onChange={props.onChange}
+        />
+    )
+};
+
+export default TextInput;
